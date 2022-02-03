c89e406f0e5210063fd2c5bc08a392d72a665d56
rumeysa-akcanca <94036242+rumeysa-akcanca@users.noreply.github.com>
2022-02-03 13:53:10 +0300

    User tasks added.
    Iclal and Rumeysa worked well as usual.
    And special thanks to Mr.  Tutakli and Mr. Guven.

--- RealEstate/Controllers/UserController.cs
+++ RealEstate/Controllers/UserController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RealEstate.DataAccess;
+using RealEstate.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,31 +12,49 @@ namespace RealEstate.Controllers
 {
     public class UserController : Controller
     {
+        //private UserDAL _userDAL;
+        //public UserDAL(UserDAL userDAL)
+        //{
+        //    _userDAL = userDAL;
+        //}
         // GET: User
         public ActionResult Index()
         {
-            return View();
+
+            var result = UserDAL.Methods.List();
+
+            // var users = JsonConvert.SerializeObject(UserDAL.Methods.ListUser(query));
+
+            return View(result);
         }
 
-        // GET: User/Details/5
+
+        //get: user/details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var result = UserDAL.Methods.GetByID(id);
+            return View(result);
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
+
             return View();
+
+
         }
 
         // POST: User/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(User user)
         {
             try
             {
                 // TODO: Add insert logic here
+                var insertedID = UserDAL.Methods.Insert(user);
+                user.ID = Convert.ToInt32(insertedID);
+                FotoUpload(user);
 
                 return RedirectToAction("Index");
             }
@@ -45,15 +67,19 @@ namespace RealEstate.Controllers
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var result = UserDAL.Methods.GetByID(id);
+
+            return View(result);
         }
 
         // POST: User/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, User user)
         {
             try
             {
+                UserDAL.Methods.Update(user);
+
                 // TODO: Add update logic here
 
                 return RedirectToAction("Index");
@@ -67,13 +93,29 @@ namespace RealEstate.Controllers
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var result = UserDAL.Methods.GetByID(id);
+
+            return View(result);
+        }
+        [NonAction] // Bu metot controller acction'ı değildir.
+        private void FotoUpload(User user)
+        {
+            string userPath = Server.MapPath($"~/UploadedFiles/User/{user.ID }/");
+            if (!Directory.Exists(userPath))
+            {
+                Directory.CreateDirectory(userPath);
+            }
+            string fotoPath = Path.Combine(userPath, Path.GetFileName(user.ProfilePic.FileName));
+            user.ProfilePic.SaveAs(fotoPath);
+            user.ProfilePicUrl = user.ID + "/" + user.ProfilePic.FileName;
+            UserDAL.Methods.Update(user);
         }
 
         // POST: User/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(User user)
         {
+            UserDAL.Methods.Delete(user);
             try
             {
                 // TODO: Add delete logic here
--- RealEstate/DataAccess/UserDAL.cs
+++ RealEstate/DataAccess/UserDAL.cs
@@ -10,22 +10,31 @@ namespace RealEstate.DataAccess
 {
     public class UserDAL
     {
-        static private UserDAL _Methods { get; set; }
-        static public UserDAL Methods
+        private static UserDAL _Methods { get; set; }
+
+        public static UserDAL Methods
         {
             get
             {
-                if (_Methods != null)
+                if (_Methods == null)
                     _Methods = new UserDAL();
                 return _Methods;
             }
         }
+
+        public User GetByID(int userId)
+        {
+            string query = $"SELECT * FROM Users WHERE ID={userId};";
+
+            return ListUser(query)[0];
+        }
+
         public object Insert(User user)
         {
 
-            user.AddressID = Convert.ToInt32(AddressDAL.Methods.Insert(user.Address));
+            //user.AddressID = Convert.ToInt32(AddressDAL.Methods.Insert(user.Address));
             string query = $@"
-            INSERT INTO [dbo].[User]
+            INSERT INTO [dbo].[Users]
                ([FullName]
                ,[Email]
                ,[Password]
@@ -34,20 +43,20 @@ namespace RealEstate.DataAccess
                ,[AddressID]
                ,[RoleID])
             VALUES
-           ( '{user.FullName}','{user.Email}','{user.Password}','{user.PhoneNumber}','{user.ProfilePic}',{user.AddressID},{user.RoleID});";
-            
+           ( '{user.FullName}','{user.Email}','{user.Password}','{user.PhoneNumber}','{user.ProfilePicUrl}',{user.AddressID},{user.RoleID}); SELECT CAST(scope_identity() AS int);";
+
             return DbTools.Connection.Create(query);
         }
         public User Read(int userid)
         {
-            string query = $"SELECT * FROM User WHERE ID = {userid}";
+            string query = $"SELECT * FROM Users WHERE ID = {userid}";
             User usr = ListUser(query)[0];
             return usr;
         }
         public bool Update(User user)
         {
             string query = $@"
-                            UPDATE [dbo].[User]
+                            UPDATE [dbo].[Users]
                                SET [FullName] = '{user.FullName}'
                                   ,[Email] = '{user.Email}'
                                   ,[Password] = '{user.Password}'
@@ -61,13 +70,13 @@ namespace RealEstate.DataAccess
 
         public bool Delete(User user)
         {
-            string query = $@"DELETE FROM User WHERE ID={user.ID};";
+            string query = $@"DELETE FROM Users WHERE ID={user.ID};";
             return DbTools.Connection.Execute(query);
         }
 
         public List<User> List()
         {
-            string query = "SELECT * FROM User;";
+            string query = "SELECT * FROM Users;";
             return ListUser(query);
         }
 
@@ -76,40 +85,27 @@ namespace RealEstate.DataAccess
             List<User> users = new List<User>();
             SqlCommand cmd = new SqlCommand(query, DbTools.Connection.con);
             IDataReader reader;
-            try
-            {
-                DbTools.Connection.ConnectDB();
-                reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    users.Add(new User()
-                    {
-                        ID = int.Parse(reader["ID"].ToString()),
-                        FullName = reader["FullName"].ToString(),
-                        AddressID = int.Parse(reader["AddressID"].ToString()),
-                        Email = reader["Email"].ToString(),
-                        Password = reader["Password"].ToString(),
-                        RoleID = int.Parse(reader["RoleID"].ToString()),
-                        PhoneNumber = reader["PhoneNumber"].ToString(),
-                        ProfilePicUrl = reader["ProfilePicUrl"].ToString()
-                    });
-                }
-            }
-            catch
-            {
-                Console.WriteLine("HATA OLUŞTU");
-            }
-            finally
-            {
-                DbTools.Connection.DisconnectDB();
-            }
-            return users;
-        }
 
-        public User GetByID(int id)
-        {
-            string query = $"SELECT * FROM User Where ID = {id}";
-            return ListUser(query)[0];
+            DbTools.Connection.ConnectDB();
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                users.Add(new User()
+                {
+                    ID = int.Parse(reader["ID"].ToString()),
+                    FullName = reader["FullName"].ToString(),
+                    AddressID = int.Parse(reader["AddressID"].ToString()),
+                    Email = reader["Email"].ToString(),
+                    Password = reader["Password"].ToString(),
+                    RoleID = int.Parse(reader["RoleID"].ToString()),
+                    PhoneNumber = reader["PhoneNumber"].ToString(),
+                    ProfilePicUrl = reader["ProfilePicUrl"].ToString()
+                });
+            }
+
+            DbTools.Connection.DisconnectDB();
+
+            return users;
         }
     }
 
